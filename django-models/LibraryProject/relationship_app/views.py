35ac2a1284159cf0780e71950434fde5744f7169
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-09 18:54:35 +0300

    Add LibraryDetailView for library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library  # Includes Library import
 
+# Function-based view (unchanged)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view (exact to match checker)
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'

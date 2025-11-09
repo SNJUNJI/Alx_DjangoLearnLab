dddde474f438110142cdfeac760230b40fb4d617
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-09 18:57:50 +0300

    Refactor import statements in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  # Exact line to match checker
 from .models import Book
-from .models import Library  # Exact line to match checker
+from .models import Library
 
 # Function-based view (unchanged)
 def book_list(request):

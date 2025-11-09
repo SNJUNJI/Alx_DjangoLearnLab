5a7e1f80b2a52b1cd431137202d3d19180e581d4
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-09 21:08:59 +0300

    Update models.py

--- django-models/LibraryProject/accounts/models.py
+++ django-models/LibraryProject/accounts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-#hehe
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"

e31c3020f3eeb767321d12be11c8c4a836a5869b
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-09 18:53:19 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
-    text_list = "Books Available:\n"
-    for book in books:
-        text_list += f"- {book.title} by {book.author.name}\n"
-    return HttpResponse(text_list, content_type='text/plain')
-
-# Create your views here.
+    return render(request, 'relationship_app/list_books.html', {'books': books})

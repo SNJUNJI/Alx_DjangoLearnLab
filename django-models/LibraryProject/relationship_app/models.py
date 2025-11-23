524157578d1114b4ded09d66573a1c002edd7ddf
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-23 08:41:17 +0300

    all

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,12 +3,21 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 class Book(models.Model):
     title=models.CharField
     author=models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+         return self.name
 class Library(models.Model):
     name=models.CharField()
     book=models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
 class Librarian(models.Model):
     name=models.CharField()
     library=models.OneToOneField(Library, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name

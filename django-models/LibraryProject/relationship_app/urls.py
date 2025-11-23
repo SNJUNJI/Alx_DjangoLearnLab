6420dd01b7c916fca1233052ea2af41f3a4f1e72
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-23 10:15:28 +0300

    all

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
-#from django.urls import path
-#from .views import list_books, LibraryDetailView
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
-#urlpatterns = [
-#    path("books/",list_books,name="all_books"),
-#   path("class/",LibraryDetailView.as_view(),name="all_books_in_library")
-#]
+urlpatterns = [
+    path("books/",list_books,name="all_books"),
+   path("class/",LibraryDetailView.as_view(),name="all_books_in_library")
+]

236cf4e2d8feebde9b66ba452dee670a4d355a7c
Samuel NJUNJI <njunjis14@gmail.com>
2025-11-23 09:01:35 +0300

    all

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,5 +5,5 @@ library=Library.objects.get(name="Library")
 all_books=library.objects.all()
 print(all_books)
 
-librarian=Library.object.get(name=Library_name)
+librarian=Library.object.get(name=Library_name), books.all()
 print (librarian)
